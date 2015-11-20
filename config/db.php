7865213838542c085e08408666a161537fa3792a
Sanmao Wang <wang@sanmao.me>
2015-11-20 16:16:00 +0800

    Revert "app"
    This reverts commit 8b301e878539b764bfcfdf20e5ba6b7d10725272.

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=db_energy',
     'username' => 'root',
-    'password' => 'ki2nway967',
+    'password' => '',
     'charset' => 'utf8',
 ];
